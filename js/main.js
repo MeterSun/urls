a255873b935745ebf8a4888fb9fc37982a8e9f76
MeterSun <zydyouxiang@hotmail.com>
2018-08-10 22:17:44 +0800

    增加可保存常用链接的功能

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ var urltxtprase = function (urlstring) {
 };
 $(function(){
     $(window).scroll(function () {
-        console.log($('#body')[0].offsetTop - document.documentElement.scrollTop);
+        // console.log($('#body')[0].offsetTop - document.documentElement.scrollTop);
         if(($('#body')[0].offsetTop - document.documentElement.scrollTop)<=0){
             $('.tagbox').addClass('leftbox-fixed')
             // $('.tagbox').css('position','fixed')
@@ -52,3 +52,35 @@ $(function(){
         
     })
 });
+
+
+var getLocalStorage = function (key) {
+    switch (localStorage.getItem('__'+key)) {
+        case 'undefined':   return undefined;   break;
+        case 'boolean':     return localStorage.getItem(key) == 'true' ? true : false;  break;
+        case 'object':      return JSON.parse(localStorage.getItem(key));   break;
+        case 'number':      return 1 * localStorage.getItem(key);   break;
+        case 'string':      return JSON.parse(localStorage.getItem(key));       break;
+        // case 'string':      return localStorage.getItem(key);       break;
+        case null:          return localStorage.getItem(key);       break;
+        default:            console.warn('undetected type');
+                            return localStorage.getItem(key);       break;
+    }
+}
+var setLocalStorage = function (key,value) {
+    localStorage.setItem('__'+key,typeof value)
+    // if(localStorage.getItem('__'+key)=='string'){
+        // localStorage.setItem(key,value)
+    // }else{
+        localStorage.setItem(key,JSON.stringify(value))
+    // }
+}
+// 初始 localstorage 无值的赋初始值
+var initLocalStorage = function(obj){
+    for (const key in obj) {
+        // localStorage.getItem(key) == undefined && localStorage.setItem(key,obj[key]);
+        // getLocalStorage(key) == undefined &&  setLocalStorage(key,obj[key]);
+        getLocalStorage(key) || setLocalStorage(key,obj[key]);
+    }
+    return obj;
+}
